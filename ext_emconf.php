1320220903372012f3dd8b9d5cebbfcd2746f988
mrimann <mrimann@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-02-23 16:18:10 +0000

    Basic kickstarter output. Makes nothing. It's just to qualify for the t3xdev-rules (present ext_emconf.php file).
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ajaxifier/trunk@8360 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "ajaxifier"
+#
+# Auto generated 23-02-2008 17:12
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Ajaxfy your TYPO3 site!',
+	'description' => 'This extension delivers the possibility to ajaxify your website.',
+	'category' => 'fe',
+	'author' => 'Mario Rimann, Sven Waechli',
+	'author_email' => 'mario@screenteam.com, sven@screenteam.com',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"beac";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"f01f";s:20:"doc/wizard_form.html";s:4:"95ed";}',
+);
+
+?>
